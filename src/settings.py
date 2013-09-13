7aa441a6205cbd91261a7d67d5947e316f5a2448
William Ting <io@williamting.com>
2013-09-13 08:53:11 -0500

    update settings for latest version of Pelican

--- src/settings.py
+++ src/settings.py
@@ -2,27 +2,23 @@
 AUTHOR = 'William Ting'
 EMAIL = 'io at williamting.com'
 SITENAME = 'William Ting'
-#TAGLINE = 'does stuff the hard way'
-SITEURL_PUBLIC = 'http://blog.williamting.com'
+#TAGLINE = 'lame tagline here :)'
+SITEURL_PUBLIC = 'http://williamting.com'
 SITEURL_LOCAL = 'http://localhost:8000'
 TIMEZONE = 'America/Chicago'
 DEFAULT_LANG = 'en'
 LOCALE = ''
 DEFAULT_DATE_FORMAT = ('%b %d, %Y')
 
-#THEME = 'notmyidea'
 THEME = './src/themes/svbtle/'
 DEFAULT_CATEGORY = 'misc'
 ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 RELATIVE_URLS = False
-MARKUP = 'md'
-
-PLUGINS = ["pelican.plugins.latex"]
-LATEX = 'article'
+MARKUP = ['md']
 
 FEED_DOMAIN = SITEURL
-FEED_ATOM = 'feeds/all.atom.xml'
+# FEED_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/category.%s.atom.xml'
 TAG_FEED_ATOM = 'feeds/tag.%s.atom.xml'
 
@@ -36,7 +32,7 @@ LINKS = (
         ('@_wting', 'https://twitter.com/_wting'),
         ('email', 'mailto:william.h.ting at gmail.com'),
         ('github', 'https://github.com/wting'),
-        ('feed', FEED_DOMAIN + '/' + FEED_ATOM),
+        # ('feed', FEED_DOMAIN + '/' + FEED_ATOM),
         )
 
 DEFAULT_PAGINATION = 5
@@ -44,10 +40,13 @@ REVERSE_ARCHIVE_ORDER = True
 DISPLAY_PAGES_ON_MENU = True
 
 # static paths will be copied under the same name
-STATIC_PATHS = ["images", ]
+STATIC_PATHS = [
+        'images',
+        'images/favicon.ico',
+        'robots.txt',
+        ]
 
-# A list of files to copy from the source to the destination
-FILES_TO_COPY = (
-        ('robots.txt', 'robots.txt'),
-        ('images/favicon.ico','favicon.ico'),
-        )
+EXTRA_PATH_METADATA = {
+        'robots.txt': {'path': 'robots.txt'},
+        'images/favicon.ico': {'path': 'favicon.ico'},
+        }
